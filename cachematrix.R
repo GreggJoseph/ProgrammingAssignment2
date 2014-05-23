d53301e21de18fb8b5cb734b3cc44f2fea1ab646
Gregg Fenton <gfenton@ara.com>
2014-05-22 20:32:01 -0600

    Added better comments and a tester code for the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
 
 ## makeCacheMatrix creates a list of functions that :
-## 1. A Setter function that sets the value of the matrix 'x'
-## 2. A Getter function that gets the matrix 'x'
+## 1. A Setter function that sets the value of the data matrix 'x'
+## 2. A Getter function that gets the data matrix 'x'
 ## 3. A Setter function sets the values of the inverse of the 'x' matrix
 ## 4. A Getter function that gets the inverse values of the matrix 'x'
 makeCacheMatrix <- function(x = matrix()) {
         
-        # inv_x will store the cached inverse matrix
-        inv_x <- NULL
+        # inverse_x  will store the cached inverse matrix
+        inverse_x <- NULL
         
-        # Setter for the matrix
+        # Setter for the data matrix 'x'
         set <- function(y) {
                 x <<- y
-                inv_x <<- NULL
+                inverse_x <<- NULL
         }
-        # Getter for the matrix
+        # Getter for the data matrix 'x'
         get <- function() x
         
         
         # Setter for the inverse
-        setinverse <- function(inverse) inv_x <<- inverse
+        setinverse <- function(inverse) inverse_x <<- inverse
         # Getter for the inverse
-        getinverse <- function() inv_x
+        getinverse <- function() inverse_x
         
         
         # Return the matrix with our newly defined functions
@@ -38,22 +36,22 @@ makeCacheMatrix <- function(x = matrix()) {
 ##   makeCacheMatrix function.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        inv_x <- x$getinverse()
+        inverse_x <- x$getinverse()
         
         # If the inverse is already calculated, return it
-        if (!is.null(inv_x)) {
+        if (!is.null(inverse_x)) {
                 message(" >>> Getting cached data...")
-                return(inv_x)
+                return(inverse_x)
         }
         
         # The inverse is not yet calculated, so compute the inverse using solve()
         #  Assume the supplied matrix is square...
         data <- x$get()
-        inv_x <- solve(data, ...)
+        inverse_x <- solve(data, ...)
         
         # Cache the inverse
-        x$setinverse(inv_x)
+        x$setinverse(inverse_x)
         
         # Return inverse matrix
-        return(inv_x)
+        return(inverse_x)
 }
--- test-driver.R
+++ test-driver.R
@@ -5,8 +5,13 @@ rm(list = ls()) # Clears the runtime envirnoment
 setwd("~/Google Drive/Learning/R_Programming/Assignments/ProgrammingAssignment2")
 source("cachematrix.R")
 
-x <- matrix(rnorm(64), nrow = 8)  # Create a matrix x
-cx <- makeCacheMatrix(x)          # Create our special matrix
-cx$get()                          # Return the matrix
-cacheSolve(cx)                    # Return the inverse
-cacheSolve(cx)                    # Call the 2nd time, so return the cached inverse
+# Create data matrix x
+x <- matrix(rnorm(49), nrow = 7) 
+# Create cache matrix
+xdata <- makeCacheMatrix(x)
+# Return data matrix
+xdata$get() 
+# Get the data matrix inverse
+cacheSolve(xdata)
+# Call again to test the cached data return functionality
+cacheSolve(xdata)
